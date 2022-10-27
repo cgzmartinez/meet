9f00deb492dd6c7e1cfccabe2f2de690ebe65ceb
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2022-10-27 13:31:03 -0400

    Acceptance Testing with features

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -2,18 +2,17 @@ import { loadFeature, defineFeature } from 'jest-cucumber';
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import App from '../App';
-import CitySearch from '../CitySearch';
 import { mockData } from '../mock-data';
+import CitySearch from '../CitySearch';
+import { extractLocations } from '../api';
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
 defineFeature(feature, test => {
-
   test('When user hasn’t searched for a city, show upcoming events from all cities.', ({ given, when, then }) => {
     given('user hasn’t searched for any city', () => {
 
     });
-
     let AppWrapper;
     when('the user opens the app', () => {
       AppWrapper = mount(<App />);
@@ -27,12 +26,13 @@ defineFeature(feature, test => {
 
   test('User should see a list of suggestions when they search for a city', ({ given, when, then }) => {
     let CitySearchWrapper;
+    let locations = extractLocations(mockData);
     given('the main page is open', () => {
       CitySearchWrapper = shallow(<CitySearch updateEvents={() => { }} locations={locations} />);
     });
 
     when('the user starts typing in the city textbox', () => {
-      CitySearchWrapper.find('.city').simulate('change', { target: { value: 'Berlin' } });
+      CitySearchWrapper.find('.city').simulate('change', { target: { value: 'Berlin' } })
     });
 
     then('the user should receive a list of cities (suggestions) that match what they’ve typed', () => {
@@ -66,5 +66,4 @@ defineFeature(feature, test => {
       expect(AppWrapper.find('.event')).toHaveLength(mockData.length);
     });
   });
-
 });
