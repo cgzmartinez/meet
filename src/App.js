78b9236f5665dadcc7b4324d1b283754ee503ebc
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2022-10-30 22:07:39 -0400

    Created scatterplot and piechart
    Installed Recharts and created EventGenre.js to add scatterplot and piechart

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import WelcomeScreen from './WelcomeScreen';
 import { OfflineAlert } from './Alert';
+import EventGenre from './EventGenre';
 import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
-
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 class App extends Component {
   state = {
@@ -68,16 +69,45 @@ class App extends Component {
     });
   };
 
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return { city, number };
+    })
+    return data;
+  };
+
   render() {
     if (this.state.showWelcomeScreen === undefined) return <div
       className="App" />
+    const { locations, numberOfEvents, events } = this.state;
     return (
       <div className="App">
-        <OfflineAlert text={this.state.warningText} />
         <h2>MeetApp</h2>
-        <CitySearch className="CitySearch" locations={this.state.locations} updateEvents={this.updateEvents} />
-        <NumberOfEvents className="numberOfEvents" updateEvents={this.updateEvents} events={this.state.events} />
-        <EventList events={this.state.events} />
+        <OfflineAlert text={this.state.warningText} />
+        <CitySearch className="CitySearch" locations={locations} updateEvents={this.updateEvents} />
+        <NumberOfEvents className="numberOfEvents" numberOfEvents={numberOfEvents} updateEvents={this.updateEvents} />
+        <h4>Events in each city</h4>
+        <div className="data-vis-wrapper">
+          <EventGenre events={events} />
+          <ResponsiveContainer height={400} >
+            <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+              <CartesianGrid />
+              <XAxis type="category" dataKey="city" name="city" />
+              <YAxis
+                allowDecimals={false}
+                type="number"
+                dataKey="number"
+                name="number of events"
+              />
+              <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+              <Scatter data={this.getData()} fill="#9368B7" />
+            </ScatterChart>
+          </ResponsiveContainer>
+        </div>
+        <EventList events={events} updateEvents={this.updateEvents} numberOfEvents={this.state.numberOfEvents} />
         <WelcomeScreen
           showWelcomeScreen={this.state.showWelcomeScreen}
           getAccessToken={() => { getAccessToken() }} />
